fc738126b5b0231c280de47a666c6d802fed79ea
Maria Paula Toledo <maripaulaa6@gmail.com>
2026-06-15 20:54:00 -0300

    Organização e criação menu de jogo
    Menu criado com animação em javascript e css;
    
    Reorganização de pastas e arquivos;
    
    Criação da animação do botão de start.
    
    Obs: a parte em html do menu deve estar presente em todas as páginas do projeto e a parte css deve ser linkada em todas as páginas também.

---
+++ Javascript/script.js
@@ -0,0 +1,47 @@
+//Menu 
+
+const abrirMenu = document.querySelector("#abrirMenu, #btnMenu"); //#abrirMenu - index | #btnMenu - outras páginas 
+const menu = document.querySelector("#menu");
+const overlay = document.querySelector("#overlay");
+
+abrirMenu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+    overlay.classList.toggle("ativo");
+});
+
+overlay.addEventListener("click", () => {
+    menu.classList.remove("ativo");
+    overlay.classList.remove("ativo");
+});
+
+//animação carregamento
+
+const start = document.querySelector("#start");
+const loading = document.querySelector("#loading");
+const progresso = document.querySelector("#progresso");
+
+start.addEventListener("click", (evento) => {
+
+    evento.preventDefault();
+
+    loading.style.display = "flex";
+
+    let porcentagem = 0;
+
+    let carregando = setInterval(() => {
+
+        porcentagem++;
+
+        progresso.style.width = porcentagem + "%";
+
+        if (porcentagem >= 100) {
+
+            clearInterval(carregando);
+
+            window.location.href = "PaginaInicial.html";
+
+        }
+
+    }, 30);
+
+});
